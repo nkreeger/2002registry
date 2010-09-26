03db7ab42016394f33ae5602945657723513895a
Nick kreeger <nick@songbirdnest.com>
2010-09-26 09:35:40 -0700

    Fixing unit tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,29 +1,10 @@
-# == Schema Info
-#
-# Table name: users
-#
-#  id               :integer         not null, primary key
-#  address1         :string(255)
-#  address2         :string(255)
-#  city             :string(255)
-#  country          :string(255)
-#  email            :string(255)
-#  name             :string(255)
-#  password         :string(255)
-#  password_confirm :string(255)
-#  share_info       :boolean
-#  state            :string(255)
-#  zip              :string(255)
-#  created_at       :datetime
-#  updated_at       :datetime
-
 require 'spec_helper'
 
 describe User do
   before(:each) do
     @valid_attributes = {
       :name => "value for name",
-      :email => "value for email",
+      :email => "value@test.org",
       :address1 => "value for address1",
       :address2 => "value for address2",
       :city => "value for city",
