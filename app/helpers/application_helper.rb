e6de4979657d2cb9bf078c571ca6ae26457d1663
Nick kreeger <nick@songbirdnest.com>
2010-10-03 11:12:25 -0700

    Adding sign in popup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,20 @@ module ApplicationHelper
     image_tag("banner.jpg", :alt => "BWM 2002 Registry", :class => "round")
   end
 
+
+  def javascript_files
+    files = []
+    if Rails.env.development?
+      files += ["vendor/jquery-1.4.2.js",
+                "vendor/jquery.hoverIntent.js"]
+    else
+      files += ["vendor/jquery-1.4.2.min.js",
+                "vendor/jquery.hoverIntent.min.js"]
+    end
+    
+    files += ["menu_popup"]
+    files
+  end
+
+
 end
---
+++ public/javascripts/menu_popup.js
@@ -0,0 +1,52 @@
+(function($) {
+  $.fn.menuPopup = function(popupMenuSelector) {
+    if ($(this).length == 0) {
+      return;
+    }
+
+    var popupMenuElement = $(popupMenuSelector);
+    var popupMenuLink = $(this);
+
+    $(document).click(function(event) {
+      if ($(event.target).parents(popupMenuSelector).length == 0 &&
+          !$(event.target).is(popupMenuSelector))
+      {
+        popupMenuElement.hide();
+        popupMenuLink.removeClass("active");
+      }
+    });
+
+    popupMenuLink.click(function() {
+      $("body").append(popupMenuElement);
+      placePopupMenu(popupMenuLink, popupMenuElement);
+      
+      if (popupMenuElement.is(":visible")) {
+        popupMenuLink.removeClass("active");
+        popupMenuElement.hide();
+      }
+      else {
+        popupMenuLink.addClass("active");
+        popupMenuElement.show();
+      }
+      return false;
+    });
+
+    $(window).resize(function() {
+      placePopupMenu(popupMenuLink, popupMenuElement);
+    });
+
+    function placePopupMenu(popupMenuLink, popupMenuElement) {
+      popupMenuElement.css("left", "0px");
+      if (popupMenuLink.offset().left + popupMenuElement.outerWidth() > $(window).width()) {
+        var leftPos = (popupMenuLink.offset().left + popupMenuLink.outerWidth() - popupMenuElement.outerWidth());
+        popupMenuElement.css("left", leftPos + "px");
+      }
+      else {
+        popupMenuElement.css("left", popupMenuLink.offset().left + "px");
+      }
+
+      console.log(" -- Setting top: " + popupMenuLink.offset().top + popupMenuLink.height() + 10 + "px");
+      popupMenuElement.css("top", popupMenuLink.offset().top + popupMenuLink.height() + 10 + "px");
+    }
+  };
+})(jQuery);
