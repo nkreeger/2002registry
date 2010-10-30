1095b30a52c767675787bd2360804cbb6a53072e
Nick kreeger <nick.kreeger@rd.io>
2010-10-30 08:38:51 -0700

    Finish up actual registration

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -4,4 +4,11 @@ class CarsController < ApplicationController
     @car = Car.new
   end
 
+  def create
+    car = Car.create!(params[:car])
+    model = Model.find_by_id(car.model_id)
+    flash[:notice] = "'#{car.vin}' a #{car.claimed_year} #{model.name} has been added to the registry."
+    redirect_to current_user
+  end
+
 end
