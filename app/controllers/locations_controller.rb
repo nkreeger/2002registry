056164c2e7fb3e183cf6d4fc90add609a35c913b
Nick kreeger <nick.kreeger@rd.io>
2010-11-02 10:05:26 -0700

    Fix up the locations stuff

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,9 +2,11 @@ require "geokit"
 
 class LocationsController < ApplicationController
   def lookup
-    Rails.logger.info(Geokit::Geocoders::IpGeocoder.do_geocode(request.remote_ip).inspect)
+    location = Geokit::Geocoders::IpGeocoder.do_geocode(request.remote_ip)
     render :json => {
-      :success => true
+      :success => (!location.country_code.nil? && location.country_code != "XX"),
+      :country_code => location.country_code,
+      :state_code => location.state
     }
   end
 end
--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -3,15 +3,26 @@ require "spec_helper"
 describe LocationsController do
   describe "GET my current location" do
     it "should return a JSON response with some location information" do
+      Geokit::Geocoders::IpGeocoder.should_receive(:do_geocode).and_return(
+          Geokit::GeoLoc.new(:city => "San Francisco", :state => "CA", :country_code => "US"))
+
       get :lookup
       response.should be_success
       
       json_response = ActiveSupport::JSON.decode(response.body.as_json)
       json_response["success"].should be_true
-#
-# NOT SURE WHAT TO DO HERE EXACTLY
-#      json_response["location"].should == "CA"
-#
+      json_response["country_code"].should == "US"
+      json_response["state_code"].should == "CA"
+    end
+
+    it "should return false if no location information is available" do
+      get :lookup
+      response.should be_success
+
+      json_response = ActiveSupport::JSON.decode(response.body.as_json)
+      json_response["success"].should be_false
+      json_response["country_code"].should be_nil
+      json_response["state_code"].should be_nil
     end
   end
 end
