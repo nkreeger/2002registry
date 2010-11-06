35d2012d5dee8996faabb0adb8d3858f91312feb
Nick kreeger <nick.kreeger@rd.io>
2010-11-06 09:44:02 -0700

    Fixing up users show page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
 describe UsersController do
+  describe "GET new" do
+    it "should setup a new user variable" do
+      get :new
+      response.should be_success
+      assigns(:title).should == "Sign up"
+      assigns(:user).should_not be_nil
+    end
+  end
+
   describe "GET show" do
-  
     it "should have a landing page for the user once they sign in" do
       user = mock_model(User, :email => "test@user.org", :password => "foobar")
       User.should_receive(:find).with("1").and_return(user)
@@ -10,6 +18,41 @@ describe UsersController do
       get :show, :id => "1"
       response.should be_success
     end
-  
+  end
+
+  describe "POST create" do
+    it "should create add a new user account if the correct information is used" do
+      mock_user = mock_model(User,
+                             :name => "Test User",
+                             :email => "test@test.com",
+                             :address1 => "123 Main St.",
+                             :city => "San Francisco",
+                             :state => "CA",
+                             :zip => 94105,
+                             :country => "US",
+                             :password => "foobar",
+                             :password_confirm => "foobar",
+                             :share_info => true)
+      User.should_receive(:new).and_return(mock_user)
+      User.should_receive(:save).and_return(true)
+
+      post :create, {
+        :user => {
+          :name => mock_user.name,
+          :email => mock_user.email,
+          :address1 => mock_user.address1,
+          :address2 => mock_user.address2,
+          :city => mock_user.city,
+          :state => mock_user.state,
+          :zip => mock_user.zip,
+          :country => mock_user.country,
+          :password => mock_user.password,
+          :password_confirm => mock_user.password_confirm
+        }
+      }
+    end
+
+    it "should not add a new user account if the incorrect information is used" do
+    end
   end
 end
