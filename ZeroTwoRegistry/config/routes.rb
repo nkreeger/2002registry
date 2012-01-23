7e9e195db11046a1c36d6930007140ca6cc71c90
Nick Kreeger <nick.kreeger@rd.io>
2012-01-23 11:06:32 -0800

    Parking wip - just got the users controller and model laid in.

--- ZeroTwoRegistry/config/routes.rb
+++ ZeroTwoRegistry/config/routes.rb
@@ -2,6 +2,8 @@ ZeroTwoRegistry::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  resources :users
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
--- ZeroTwoRegistry/db/migrate/20120123184448_create_users.rb
+++ ZeroTwoRegistry/db/migrate/20120123184448_create_users.rb
@@ -1,7 +1,26 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string   "name"
+      t.string   "email"
+      t.string   "address1"
+      t.string   "address2"
+      t.string   "city"
+      t.string   "state"
+      t.string   "zip"
+      t.string   "country"
+      t.string   "password"
+      t.string   "password_confirm"
+      t.boolean  "share_info"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.string   "encrypted_password"
+      t.string   "salt"
+      t.string   "remember_token"
+      t.string   "avatar_file_name"
+      t.string   "avatar_content_type"
+      t.integer  "avatar_file_size"
+      t.datetime "avatar_updated_at"
       t.timestamps
     end
   end
