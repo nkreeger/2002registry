2eac1eae59512c02a8d48a4d837f14d6136d5bd2
Nick Kreeger <nick.kreeger@rd.io>
2012-01-23 10:29:40 -0800

    Move old site to old folder. Start new site.

---
+++ ZeroTwoRegistry/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZeroTwoRegistry::Application.config.secret_token = '823b8209e619cdf599b2e9a21aaab270a77c1a2b266246ad5cf3b048b709970b43256b772a27c830e0952dc1f75469d61825b1a09cc3057301fec62eb83e7897'
