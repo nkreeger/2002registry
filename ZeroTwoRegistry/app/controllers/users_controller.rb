d1a53cec390727cf1a6354641182a447fb3e6f36
Nick Kreeger <nick.kreeger@rd.io>
2012-01-24 10:23:47 -0800

    Fill out User controller and add the ApplicationHelper methods.

--- ZeroTwoRegistry/app/controllers/users_controller.rb
+++ ZeroTwoRegistry/app/controllers/users_controller.rb
@@ -4,4 +4,51 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @User
+      flash[:notice] = "Welcome to the 2002 registry #{@user.name}"
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if current_user.update_attributes(params[:user])
+      flash[:notice] = "#{current_user.name} your profile has been updated!"
+      redirect_to current_user
+    else
+      flash[:error] = "Could not update your profile!"
+      render "edit"
+    end
+  end
+
+  def edit_image
+    render :layout => false
+  end
+
+  def update_image
+    current_user.avatar = params[:user][:avatar]
+    current_user.save!
+    redirect_to current_user
+  end
+
+  def change_password
+    render :layout => false
+  end
+
+  def update_password
+    # XXX VALIDATE HERE!
+    current_user.update_password!(params[:old_password], params[:new_password])
+    redirect_to current_user
+  end
 end
--- ZeroTwoRegistry/app/helpers/application_helper.rb
+++ ZeroTwoRegistry/app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  def banner
+    image_tag("banner.jpg", :alt => "BWM 2002 Registry", :class => "round")
+  end
+
+  def javascript_files
+    files = []
+    if Rails.env.development?
+      files += ["vendor/jquery-1.4.2.js",
+                "vendor/jquery.hoverIntent.js"]
+    else
+      files += ["vendor/jquery-1.4.2.min.js",
+                "vendor/jquery.hoverIntent.min.js"]
+    end
+    
+    files += ["application", "dialog", "menu_popup", "registry"]
+    files
+  end
+
 end
